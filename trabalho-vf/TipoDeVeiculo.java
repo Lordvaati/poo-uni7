412a0716343acf0425a453f10260718e9c70e720
Gabriel Cruz <72208333+Lordvaati@users.noreply.github.com>
2021-06-16 23:35:28 +0000

    trabalho-vf

--- trabalho-vf/TipoDeVeiculo.java
+++ trabalho-vf/TipoDeVeiculo.java
@@ -1,5 +1,6 @@
 public enum TipoDeVeiculo {
 /* Princípio da Segregação da Interface — Da maneira que está implementada bicleta é obrigada a chamar motor, mesmo que a maioria das bicicletas não tem motor, então cada Tipo de viculo deve ser classes unicas */
+/* Princípio Aberto-Fechado - toda vez que um novo veiculo for adicionado a classe vai ser mudada*/
   CARRO, 
   CARRO_ELETRICO,
   BICICLETA
